3eba1f60611e67f0a31ed7de1c0157accd50ea50
xubo <bo.xu@ifchange.com>
2019-09-25 17:03:42 +0800

    add now time

--- yourbasic/datetime_format.go
+++ yourbasic/datetime_format.go
@@ -10,4 +10,5 @@ func main() {
 	t, _ := time.Parse("2006-01-02 15:04:05", timeStr)
 	fmt.Println(t.Format("2006-01-02 15:04:05"))
 	fmt.Println(timeStr)
+	fmt.Println("now the time is ", time.Now().Format("2006-01-02 15:04:05"))
 }
